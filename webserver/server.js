f4d6741379d4fd0c935ab34ceb13d503a52ee1c1
jennormous <smj.boudreau@gmail.com>
2020-03-07 15:45:11 -0800

    connected mongoDB

--- webserver/server.js
+++ webserver/server.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const { port, entry } = require('./globals');
-const path= require('path');
-const bodyParser = require('body-parser');
-
+const express = require("express");
+const { port, entry } = require("./globals");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const server = express();
 
-const db = require("./config/keys").mongoURI;
+const db = require("./config/key").mongoURI;
 mongoose
   .connect(db, {
     useUnifiedTopology: true,
@@ -16,45 +16,42 @@ mongoose
   .then(() => console.log("connected to DB!"))
   .catch((err) => console.log(err));
 
-
-server.use(express.static(path.join(__dirname, 'statics')));
-server.use(bodyParser.urlencoded({extended: true}));
+server.use(express.static(path.join(__dirname, "statics")));
+server.use(bodyParser.urlencoded({ extended: true }));
 
 // let userAccount = {'account': (req.body.account)}
 // let userGoal= {'goal': (req.body.goal)}
 // let userTime = {'time': (req.body.time)}
 // let userEta = {'eta': (req.body.eta)};
 
-server.set('view engine', 'ejs');
+server.set("view engine", "ejs");
 
-server.get('/', (req, res)=>{
-    let newMessage = 'hello'
-    res.render('hello', {
-        newMessage : newMessage
-    })
-})
+server.get("/", (req, res) => {
+  let newMessage = "hello";
+  res.render("hello", {
+    newMessage: newMessage
+  });
+});
 
-server.get('/form', (req, res)=>{
-    res.send('This is the form')
-})
+server.get("/form", (req, res) => {
+  res.send("This is the form");
+});
 
-server.get('/finished', (req,res)=>{
-    res.send('finished page')
-})
+server.get("/finished", (req, res) => {
+  res.send("finished page");
+});
 
-server.post('/withdraw', (req,res)=>{
-    let wdAmount = {'wdAmount': (req.body.wdAmount)};
-    
+server.post("/withdraw", (req, res) => {
+  let wdAmount = { wdAmount: req.body.wdAmount };
 
+  // res.redirect('/finished')
+});
 
-    // res.redirect('/finished')
-})
+server.get("/deposit", (req, res) => {
+  res.send("this is deposit");
+  // res.redirect('/finished')
+});
 
-server.get('/deposit', (req,res)=>{
-    res.send('this is deposit')
-    // res.redirect('/finished')
-
-})
-
-
-server.listen( port, () => console.log( `\nServer live at http://localhost:${port}` ) )
+server.listen(port, () =>
+  console.log(`\nServer live at http://localhost:${port}`)
+);
